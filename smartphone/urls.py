6b166e5db79df750ee3d75fadda25f8d2020d145
KarimovProject <abdullatifkarimov772@gmail.com>
2022-10-27 02:41:28 +0500

    urls is ready

--- smartphone/urls.py
+++ smartphone/urls.py
@@ -1,24 +1,13 @@
-"""smartphone URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from turtle import home
 from django.contrib import admin
 from django.urls import path
+from django.views import View
 from smartphone_app.views import (
 
     get_all_company,
     get_company_by_name,
+    Homeview
 
          
 
@@ -28,7 +17,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('company/', get_all_company),
     path('company/<str:company>/', get_company_by_name, name='company'),
-
-
-
+    path('', Homeview.as_view()),
 ]
